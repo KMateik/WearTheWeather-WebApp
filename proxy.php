fb47deaa2286b757e0b0906d44d2cf04f72bb8eb
KMateik <49290520+KMateik@users.noreply.github.com>
2019-04-25 16:31:20 -0400

    Add files via upload

--- proxy.php
+++ proxy.php
@@ -1,13 +1,13 @@
 <?php
-$api_key = '48030fcc9da4d24cc0ddb633dfae0e09';
-$API_ENDPOINT = 'https://api.darksky.net/forecast/';
+$api_key = "48030fcc9da4d24cc0ddb633dfae0e09";
+$API_ENDPOINT = "https://api.darksky.net/forecast/";
 
 $lat = $_REQUEST["lat"];
 $lng = $_REQUEST["lng"];
 
-$url = $API_ENDPOINT . $api_key . '/' . $lat . '/' . $lng;
+$url = $API_ENDPOINT . $api_key . '/' . $lat . ',' . $lng;
 
-$response = file_get_contents(urlencode($url));
+$response = file_get_contents($url);
 #if(!isset($_GET['url'])) die();
 #$url = $url . $_GET['url'];
 #$url = file_get_contents($url);
